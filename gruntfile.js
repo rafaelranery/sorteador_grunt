78cbb5e25698ab7b92cfcc4fd1aa5f53ab735af5
rafaelranery <rafaelrapnery@gmail.com>
2023-03-09 10:54:33 -0300

    Aula 3: Usando Grunt para compilar LESS e SASS

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,34 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        less:{
+            /* development // podemos criar diferentes ambientes (development, production) */
+            development: {
+                files: {
+                    /* arquivo destino */
+                    'main.css':'main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+
+                },
+                files: {
+                    'main.min.css':'main.less'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'main2.css':'main.scss'
+                }
+            } 
+        }
     })
     
     /* Função para adição de tarefas */
@@ -13,6 +41,10 @@ module.exports = function(grunt) {
         }, 3000);
     });
 
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+
     /* Função default */
-    grunt.registerTask('default', ['olaGrunt']);
+    grunt.registerTask('default', ['less', 'sass']);
 }
