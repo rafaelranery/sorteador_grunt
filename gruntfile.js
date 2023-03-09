81004bbe6d943cd1d898cbb748195bc37020dd27
rafaelranery <rafaelrapnery@gmail.com>
2023-03-09 09:55:22 -0300

    Aula 2: Criação de tarefas

--- gruntfile.js
+++ gruntfile.js
@@ -2,4 +2,17 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json')
     })
+    
+    /* Função para adição de tarefas */
+    grunt.registerTask('olaGrunt', function(){
+        const done =this.async();
+        /* Simulação de função assíncrona */
+        setTimeout(() => {
+            console.log('olá grunt')
+            done();
+        }, 3000);
+    });
+
+    /* Função default */
+    grunt.registerTask('default', ['olaGrunt']);
 }
