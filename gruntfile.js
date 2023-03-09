1ab2616d4038b2a49f62725f4c339ff518c4b344
rafaelranery <rafaelrapnery@gmail.com>
2023-03-09 15:41:04 -0300

    Aula 7: Comprimindo HTMl com Grunt

--- gruntfile.js
+++ gruntfile.js
@@ -23,8 +23,62 @@ module.exports = function(grunt) {
             less: {
                 files: ['src/styles/**/*.less'],
                 tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev'],
             }
-        }
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDEREÇO_CSS',
+                            replacement: './styles/main.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDEREÇO_CSS',
+                            replacement: './styles/main.min.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['preBuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                },
+                files: {
+                    'preBuild/index.html':'src/index.html'
+                }
+            }
+        },
+        clean: ['preBuild'],
     })
     
 
@@ -32,10 +86,13 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
 
     /* Função default */
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
 
 }
